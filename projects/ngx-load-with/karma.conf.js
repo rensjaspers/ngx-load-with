4c1cfe2a1df4eff82c1071e13e5b7a53c1645c6b
Rens Jaspers <rens.jaspers@gmail.com>
2023-05-18 20:27:09 +0200

    ci: fix coverage dir

--- projects/ngx-load-with/karma.conf.js
+++ projects/ngx-load-with/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "../../coverage/ngx-data-loader"),
+      dir: require("path").join(__dirname, "../../coverage/ngx-load-with"),
       subdir: ".",
       reporters: [
         { type: "html" },
