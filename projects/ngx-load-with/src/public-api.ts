582353b04dcdb2ae9fa3089645789da091d4f99b
Rens Jaspers <rens.jaspers@gmail.com>
2023-05-18 16:43:28 +0200

    fix: public api exports

--- projects/ngx-load-with/src/public-api.ts
+++ projects/ngx-load-with/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-load-with
  */
 
-export * from './lib/ngx-load-with.service';
-export * from './lib/ngx-load-with.component';
+export * from './lib/ngx-load-with.directive';
 export * from './lib/ngx-load-with.module';
